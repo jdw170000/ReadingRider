95bd5a84b908784e064c64edbe305ecae10e9e19
Jacob Wiedemeier <jacobw-gitkraken@jandjw.com>
2021-10-02 14:53:39 -0500

    Bugfixes, path reading

--- Mainest.py
+++ Mainest.py
@@ -2,12 +2,88 @@
 import pyglet
 from pyglet.gl import *
 from pyglet.window import key
+from pyglet.window import Window
+from pyglet import app
 import math
+import json
+import os
 
-def MoveCheckCollisions(velocity, rotation, posX, posY):
+#Car vars
+turning = 0
+acceleration = 0
+velocity = 0
+rotation = math.pi # [0, 2pi)
+posX = 0
+posY = 0
+
+def MoveCheckCollisions():
     #TODO - collision checking
+    global velocity
+    global rotation
+    global posX
+    global posY
     posX += velocity * math.cos(rotation + math.pi / 2)
     posY += velocity * math.sin(rotation + math.pi / 2)
+
+def DrawPath():
+    file = open(os.getcwd()+"/paths/UTD_ALERT.path")
+    pathJson = json.load(file)
+
+def DefineWindowEvents(window):
+    @window.event
+    def on_draw():
+        #this is the main loop
+        global turning
+        global acceleration
+        global velocity
+        global rotation
+        global posX
+        global posY
+        #Car movement
+        rotation += turning * 1 # Constant to be adjusted later
+        while (rotation >= 2 * math.pi):
+            rotation += -2 * math.pi
+        velocity += acceleration * 1 # constant to be adjusted later
+        MoveCheckCollisions()
+
+        #update graphics
+        DrawPath()
+        window.clear()
+        batch.draw()
+
+    @window.event
+    def on_key_press(symbol, modifiers):
+        global turning
+        global acceleration
+        if keys[key.A]:
+            if turning > -1:
+                turning += -1
+        elif keys[key.D]:
+            if turning < 1:
+                turning += 1
+        elif keys[key.W]:
+            if acceleration < 1:
+                acceleration += 1
+        elif keys[key.S]:
+            if acceleration > -1:
+                acceleration += -1
+    @window.event
+    def on_key_release(symbol, modifiers):
+        global turning
+        global acceleration
+        if keys[key.A]:
+            if turning < 0:
+                turning += 1
+        elif keys[key.D]:
+            if turning > 0:
+                turning += -1
+        elif keys[key.W]:
+            if acceleration > 0:
+                acceleration += -1
+        elif keys[key.S]:
+            if acceleration < 0:
+                acceleration += 1
+
 
 def main():
     window = Window(width=800, height=800)
@@ -18,56 +94,7 @@ def main():
     keys = key.KeyStateHandler()
     window.push_handlers(keys)
 
-    #Car vars
-    turning = 0
-    acceleration = 0
-    velocity = 0
-    posX = 0
-    posY = 0
-    rotation = math.pi # [0, 2pi)
-
-    @window.event
-    def on_draw():
-        #this is the main loop
-        #Car movement
-        rotation += turning * 1 # Constant to be adjusted later
-        while (rotation >= 2 * math.pi):
-            rotation += -2 * math.pi
-        velocity += acceleration * 1 # constant to be adjusted later
-        MoveCheckCollisions(velocity, rotation, posX, posY)
-
-        #update graphics
-        window.clear()
-        batch.draw();
-
-    @window.event
-    def on_key_press(symbol, modifiers):
-        if keys[key.A]:
-            if turning > -1:
-                turning += -1
-        elif keys[key.D]:
-            if turning < 1:
-                turning += 1
-        elif keys[key.W]:
-            if accelleration < 1:
-                acceleration += 1
-        elif keys[key.S]:
-            if accelleration > -1:
-                acceleration += -1
-    @window.event
-    def on_key_release(symbol, modifiers):
-        if keys[key.A]:
-            if turning < 0:
-                turning += 1
-        elif keys[key.D]:
-            if turning > 0:
-                turning += -1
-        elif keys[key.W]:
-            if accelleration > 0:
-                acceleration += -1
-        elif keys[key.S]:
-            if accelleration < 0:
-                acceleration += 1
+    DefineWindowEvents(window)
 
     app.run()
 
